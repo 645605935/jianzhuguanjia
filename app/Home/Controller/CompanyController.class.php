4cfd89f0c16feca8bdbda743986d41f501625df7
tengrui zhang <tengrui zhang>
2017-07-26 01:53:23 +0800

    no message

--- app/Home/Controller/CompanyController.class.php
+++ app/Home/Controller/CompanyController.class.php
@@ -13,10 +13,7 @@ class CompanyController extends CommonController{
         $this->user=$user;
     }
 
-    //实名认证
-    public function companylicense(){
-        $this->display();
-    }
+    
 
     //公司资料
     public function companyinfo(){
@@ -142,18 +139,193 @@ class CompanyController extends CommonController{
         echo json_encode($data);
     }
 
-    public function ajax_addcaseinfo111(){
+    public function ajax_del_case(){
+        global $user;
+
+        $id=$_GET['id'];
+        if($id && $user){
+
+            $res=M('Cases')->delete($id);
+            if($res){
+                $data=array();
+                $data['code']=0;
+                $data['msg']='删除成功';
+            }else{
+                $data=array();
+                $data['code']=1;
+                $data['msg']='删除失败';
+            }
+        }else{
+            $data=array();
+            $data['code']=1;
+            $data['msg']='操作失败';
+        }
+
+        echo json_encode($data);
+    }
+
+//**************************************************************
+    public function team(){
+        global $user;
+        $where=array();
+        $where['uid']=$user['id'];
+
+
+        $count      = M('Team')->where($where)->count();
+        $Page       = new \Common\Extend\Page($count,6);
+        $nowPage = isset($_GET['p'])?$_GET['p']:1;
+        $list=M('Team')->page($nowPage.','.$Page->listRows)->where($where)->select();
+        foreach ($list as $key => $value) {
+            $list[$key]['time']=date('Y-m-d',$value['time']);
+            if($value['chongyejingyan']){
+                $temp=M('Type')->find($value['chongyejingyan']);
+                $list[$key]['chongyejingyan']=$temp['title'];
+            }
+        }
+
+
+        $this->page=$Page->show();
+        $this->list=$list;
+        $this->display();
+    }
+
+    public function addteam(){
+        $chongyejingyan=M('Type')->where(array('pid'=>1341))->select();
+
+        $this->chongyejingyan=$chongyejingyan;
+        $this->display();
+    }
+
+    public function ajax_add_team(){
         global $user;
         if($_POST && $user){
             $data=array();
-            $data['company_name']=$_POST['Name'];
-            $data['company_people']=$_POST['contact'];
-            $data['contact_phone']=$_POST['TelPhone'];
-            $data['contact_email']=$_POST['Email'];
-            $data['company_province']=$_POST['AreaPpid'];
-            $data['company_city']=$_POST['AreaId'];
-            $data['company_address']=$_POST['address'];
-            $data['company_introduction']=$_POST['Introduction'];
+            $data['img']=$_POST['img'];
+            $data['truename']=$_POST['truename'];
+            $data['phone']=$_POST['phone'];
+            $data['chongyejingyan']=$_POST['chongyejingyan'];
+            $data['fuwulinian']=$_POST['fuwulinian'];
+            $data['uid']=$user['id'];
+            $data['time']=time();
+
+            $id=M('Team')->add($data);
+
+            if($id){
+                $data=array();
+                $data['code']=0;
+                $data['msg']='添加成功';
+                $data['data']=$id;
+            }else{
+                $data=array();
+                $data['code']=1;
+                $data['msg']='添加失败';
+            }
+        }else{
+            $data=array();
+            $data['code']=1;
+            $data['msg']='操作失败';
+        }
+
+        echo json_encode($data);
+    }
+
+    public function editteam(){
+        if($id=$_GET['id']){
+            $row=M('Team')->find($id);
+            $chongyejingyan=M('Type')->where(array('pid'=>1341))->select();
+            
+            $this->row=$row;
+            $this->chongyejingyan=$chongyejingyan;
+
+            $this->display();
+        }
+    }
+
+    public function ajax_edit_team(){
+        global $user;
+        if($_POST && $user){
+            $data=array();
+            $data['img']=$_POST['img'];
+            $data['truename']=$_POST['truename'];
+            $data['phone']=$_POST['phone'];
+            $data['chongyejingyan']=$_POST['chongyejingyan'];
+            $data['fuwulinian']=$_POST['fuwulinian'];
+            $data['time']=time();
+
+
+            $res=M('Team')->where(array('id'=>$_POST['id']))->save($data);
+            if($res){
+                $data=array();
+                $data['code']=0;
+                $data['msg']='编辑成功';
+            }else{
+                $data=array();
+                $data['code']=1;
+                $data['msg']='编辑失败';
+            }
+        }else{
+            $data=array();
+            $data['code']=1;
+            $data['msg']='操作失败';
+        }
+
+        echo json_encode($data);
+    }
+
+    public function ajax_del_team(){
+        global $user;
+
+        $id=$_GET['id'];
+        if($id && $user){
+
+            $res=M('Cases')->delete($id);
+            if($res){
+                $data=array();
+                $data['code']=0;
+                $data['msg']='删除成功';
+            }else{
+                $data=array();
+                $data['code']=1;
+                $data['msg']='删除失败';
+            }
+        }else{
+            $data=array();
+            $data['code']=1;
+            $data['msg']='操作失败';
+        }
+
+        echo json_encode($data);
+    }
+
+    //实名认证
+    public function companylicense(){
+        global $user;
+
+        $companyinfo=M('User')->find($user['id']); 
+
+        $province=M('Province')->select();
+        $city=M('city')->where(array('fatherid'=>$companyinfo['company_province']))->select();
+
+        $comanySize=M('Type')->where(array('pid'=>11))->select();
+
+        $this->comanySize=$comanySize;
+        $this->companyinfo=$companyinfo;
+        $this->province=$province;
+        $this->city=$city;
+        $this->display();
+    }
+
+    public function ajax_save_companylicense(){
+        global $user;
+
+        if($_POST && $user){
+            $data=array();
+            $data['company_name']=$_POST['company_name'];
+            $data['company_province']=$_POST['province'];
+            $data['company_city']=$_POST['city'];
+            $data['company_address']=$_POST['company_address'];
+            $data['company_size']=$_POST['company_size'];
+            $data['company_yingyezhizhao']=$_POST['company_yingyezhizhao'];
             $data['time']=time();
 
             $where=array();
@@ -162,26 +334,26 @@ class CompanyController extends CommonController{
             $res=M('User')->where($where)->save($data);
             if($res){
                 $data=array();
-                $data['IsSuccess']=true;
-                $data['Msg']='保存成功';
+                $data['code']=0;
+                $data['msg']='保存成功';
             }else{
                 $data=array();
-                $data['IsSuccess']=false;
-                $data['Msg']='保存失败';
+                $data['code']=1;
+                $data['msg']='保存失败';
             }
         }else{
             $data=array();
-            $data['IsSuccess']=false;
-            $data['Msg']='操作失败';
+            $data['code']=1;
+            $data['msg']='操作失败';
         }
 
         echo json_encode($data);
     }
 
-    public function team(){
+
+    public function question(){
         $this->display();
     }
-
     
   
 
--- app/Home/Controller/SafetysetController.class.php
+++ app/Home/Controller/SafetysetController.class.php
@@ -7,6 +7,10 @@ class SafetysetController extends CommonController{
 
     public function _initialize(){
         parent::_initialize();
+
+        global $user;
+        $user=session('userinfo');
+        $this->user=$user;
     }
 
     //安全设置
@@ -14,7 +18,63 @@ class SafetysetController extends CommonController{
         $this->display();
     }
 
+    public function changepassword(){
+    	global $user;
 
+    	$userinfo=M('User')->find($user['id']);
+    	$this->phone=$userinfo['phone'];
+        $this->display();
+    }
+
+    public function ajax_edit_password(){
+        global $user;
+        if($_POST && $user){
+        	$oldpassword=$_POST['oldpassword'];
+        	$password=$_POST['password'];
+        	$repassword=$_POST['repassword'];
+
+        	$userinfo=M('User')->find($user['id']);
+        	if($userinfo['password'] == md5($oldpassword)){
+        		if($oldpassword != $password){
+	        		if($password == $repassword){
+	        			$data=array();
+	        			$data['password']=md5($password);
+	        			$data['id']=$user['id'];
+	        			$data['time']=time();
+
+	        			$res=M('User')->save($data);
+	        			if($res){
+	        			    $data=array();
+	        			    $data['code']=0;
+	        			    $data['msg']='修改成功';
+	        			}else{
+	        			    $data=array();
+	        			    $data['code']=1;
+	        			    $data['msg']='修改失败';
+	        			}
+	        		}else{
+						$data=array();
+				        $data['code']=1;
+				        $data['msg']='新旧密码不一致';
+	        		}
+        		}else{
+					$data=array();
+			        $data['code']=1;
+			        $data['msg']='新旧密码不可以一样';
+        		}
+        	}else{
+        		$data=array();
+                $data['code']=1;
+                $data['msg']='当前密码不正确';
+        	}
+        }else{
+            $data=array();
+            $data['code']=1;
+            $data['msg']='操作失败';
+        }
+
+        echo json_encode($data);
+    }
   
 
 
