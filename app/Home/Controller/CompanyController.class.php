6fae99d8e2760c3a2a58738e5a34074d82c3600a
tengrui zhang <tengrui zhang>
2017-07-25 09:42:04 +0800

    no message

--- app/Home/Controller/CompanyController.class.php
+++ app/Home/Controller/CompanyController.class.php
@@ -24,14 +24,125 @@ class CompanyController extends CommonController{
     }
 
     public function caseinfo(){
+        global $user;
+        $where=array();
+        $where['uid']=$user['id'];
+
+
+        $count      = M('Cases')->where($where)->count();
+        $Page       = new \Common\Extend\Page($count,6);
+        $nowPage = isset($_GET['p'])?$_GET['p']:1;
+        $list=M('Cases')->page($nowPage.','.$Page->listRows)->where($where)->select();
+        foreach ($list as $key => $value) {
+            $list[$key]['time']=date('Y-m-d',$value['time']);
+            if($value['zizhileixing']){
+                $temp=M('Type')->find($value['zizhileixing']);
+                $list[$key]['zizhileixing']=$temp['title'];
+            }
+            if($value['banlizhouqi']){
+                $temp=M('Type')->find($value['banlizhouqi']);
+                $list[$key]['banlizhouqi']=$temp['title'];
+            }
+        }
+
+
+        $this->page=$Page->show();
+        $this->list=$list;
         $this->display();
     }
 
     public function addcaseinfo(){
+        $banlizhouqi=M('Type')->where(array('pid'=>1336))->select();
+        $zizhileixing=M('Type')->where(array('pid'=>1275))->select();
+        
+
+        $this->banlizhouqi=$banlizhouqi;
+        $this->zizhileixing=$zizhileixing;
         $this->display();
     }
 
-    public function ajax_addcaseinfo(){
+    public function ajax_add_case(){
+        global $user;
+        if($_POST && $user){
+            $data=array();
+            $data['img']=$_POST['img'];
+            $data['company_name']=$_POST['company_name'];
+            $data['zizhileixing']=$_POST['zizhileixing'];
+            $data['hezuoyewu']=$_POST['hezuoyewu'];
+            $data['banlizhouqi']=$_POST['banlizhouqi'];
+            $data['price']=$_POST['price'];
+            $data['renyuanpeizhi']=$_POST['renyuanpeizhi'];
+            $data['uid']=$user['id'];
+            $data['time']=time();
+
+            $id=M('Cases')->add($data);
+            if($id){
+                $data=array();
+                $data['code']=0;
+                $data['msg']='添加成功';
+                $data['data']=$id;
+            }else{
+                $data=array();
+                $data['code']=1;
+                $data['msg']='添加失败';
+            }
+        }else{
+            $data=array();
+            $data['code']=1;
+            $data['msg']='操作失败';
+        }
+
+        echo json_encode($data);
+    }
+
+    public function editcaseinfo(){
+        if($id=$_GET['id']){
+            $row=M('Cases')->find($id);
+            $banlizhouqi=M('Type')->where(array('pid'=>1336))->select();
+            $zizhileixing=M('Type')->where(array('pid'=>1275))->select();
+            
+            $this->row=$row;
+            $this->banlizhouqi=$banlizhouqi;
+            $this->zizhileixing=$zizhileixing;
+
+            $this->display();
+        }
+    }
+
+    public function ajax_edit_case(){
+        global $user;
+        if($_POST && $user){
+            $data=array();
+            $data['img']=$_POST['img'];
+            $data['company_name']=$_POST['company_name'];
+            $data['zizhileixing']=$_POST['zizhileixing'];
+            $data['hezuoyewu']=$_POST['hezuoyewu'];
+            $data['banlizhouqi']=$_POST['banlizhouqi'];
+            $data['price']=$_POST['price'];
+            $data['renyuanpeizhi']=$_POST['renyuanpeizhi'];
+            $data['time']=time();
+
+
+            $res=M('Cases')->where(array('id'=>$_POST['id']))->save($data);
+            if($res){
+                $data=array();
+                $data['code']=0;
+                $data['msg']='编辑成功';
+            }else{
+                $data=array();
+                $data['code']=1;
+                $data['msg']='编辑失败';
+            }
+        }else{
+            $data=array();
+            $data['code']=1;
+            $data['msg']='操作失败';
+        }
+
+        echo json_encode($data);
+    }
+
+    public function ajax_addcaseinfo111(){
         global $user;
         if($_POST && $user){
             $data=array();
--- app/Home/Controller/IndexController.class.php
+++ app/Home/Controller/IndexController.class.php
@@ -97,6 +97,36 @@ class IndexController extends CommonController{
    
 
 
+    // 上传文件
+    public function lay_upload_file(){
+        if($_FILES['file']['size']>0){
+            $upload = new \Think\Upload();// 实例化上传类
+            $upload->maxSize   =     31457280 ;// 设置附件上传大小
+            $upload->exts      =     array('zip', 'rar', 'xls','xlsx', 'doc','docx','ppt','pptx','pdf','jpg','png','jpeg','gif');// 设置附件上传类型
+            $upload->uploadReplace  = false;// 存在同名文件是否覆盖
+            $upload->autoSub   =     false;//是否启用子目录保存
+            $upload->rootPath  =     './Uploads/'; // 设置附件上传根目录
+            $upload->savePath  =     'layui/'; // 设置附件上传（子）目录
+            $upload->saveRule  =     ''; // 设置附件上传（子）目录
+             
+            // 上传文件 
+            $info   =   $upload->upload();
+
+            if(!$info) {
+                $data=array();
+                $data['code']=0;
+                $data['msg']=$upload->getError();
+            }else{
+                $img=$info['file']['savename'];
+                $data=array();
+                $data['code']=0;
+                $data['msg']='success';
+                $data['data']["src"]='/Uploads/layui/'.$img;
+            }
+
+            echo json_encode($data);
+        }
+    }
 
 
 }
