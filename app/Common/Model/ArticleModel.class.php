35bf439525ac5153ef698fe8181f142eb932f320
zhangtengrui <645605935@qq.com>
2017-07-26 11:41:39 +0800

    1111

---
+++ app/Common/Model/ArticleModel.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace Common\Model;
+use Think\Model\RelationModel;
+class ArticleModel extends RelationModel{
+	protected $_link=array(
+		'_user'=>array(
+			'mapping_type'=>self::BELONGS_TO,
+			'class_name'=>'User',
+			'foreign_key'=>'uid',
+			'as_fields' => 'truename:_truename',
+		),
+		'_type'=>array(
+			'mapping_type'=>self::BELONGS_TO,
+			'class_name'=>'Type',
+			'foreign_key'=>'type',
+			'as_fields' => 'title:type',
+		)
+	);
+}
+
+?>
---
+++ app/Common/Model/NewsModel.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace Common\Model;
+use Think\Model\RelationModel;
+class NewsModel extends RelationModel{
+	protected $_link=array(
+		'_type'=>array(
+			'mapping_type'=>self::BELONGS_TO,
+			'class_name'=>'Type',
+			'foreign_key'=>'type',
+			'as_fields' => 'title:type',
+		)
+	);
+}
+
+?>
